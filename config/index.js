e39e6ee168a8e58c83b3e8862879e394042e9eea
agmorrow <morrow.austin5@gmail.com>
2022-04-05 15:27:06 -0600

    everything working

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
+// create connection to database
 let sequelize;
 
 if (process.env.JAWSDB_URL) {

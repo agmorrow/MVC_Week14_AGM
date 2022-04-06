47cc8731de8eebbb5dfe503120de563a8d425951
agmorrow <morrow.austin5@gmail.com>
2022-04-06 00:51:18 -0600

    app.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes.js')
+const homeRoutes = require('./app.js')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
